dbba560bca73afb490c8185e010a87fa32e6e5ef
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2025-06-01 08:15:36 +0200

    fix: embeddings route

--- app/api/proxy/embeddings/route.ts
+++ app/api/proxy/embeddings/route.ts
@@ -2,6 +2,7 @@ export async function POST(request: Request) {
   const body = await request.json();
   body.model = 'text-embedding-ada-002';
   const headers = new Headers(request.headers);
+  headers.delete('content-length');
   const response = await fetch('https://api.langdock.com/openai/eu/v1/embeddings', {
     method: request.method,
     headers,
